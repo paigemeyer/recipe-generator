eb557cad0ae1407fdc4824b96b6342ef2f6844f3
paigemeyer <paigemeyer71@icloud.com>
2025-04-03 15:52:12 +0200

    added AI and axios

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
+function displayRecipe(response){
+  let recipeElement=document.querySelector("#recipe");
+recipeElement.innerHTML= response.data.answer;  
+
+}
+
+
+
+
+
 function generateRecipe(event){
 event.preventDefault();
 
-let recipeElement=document.querySelector("#recipe");
-recipeElement.innerHTML="chicken curry";
+let instruction=document.querySelector("#ready-meal");
+let apiKey= "f333f4tb5038acab6309b2fo8d0b9912";
+let prompt=` User instructions: Generate a recipe with the ${instruction.value}`;
+let context="you are a chef and you love to make meals with different ingredients. please create short/medium easy to follow recipes and please list the ingredients needed in bullet point form, make sure to follow the users instructions below in basic HTML and put the recipe name in a <strong> element";
+let apiUrl=`https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayRecipe);
 
 }
 
